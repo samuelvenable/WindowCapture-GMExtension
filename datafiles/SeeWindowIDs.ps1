683f39641f92bf10eb289903d64ab1e9097fbb91
Samuel Venable <josephvenable@verizon.net>
2025-10-26 01:29:31 -0400

    yay

--- datafiles/SeeWindowIDs.ps1
+++ datafiles/SeeWindowIDs.ps1
@@ -1,14 +1,7 @@
-$ventanas = Get-Process | Where-Object { $_.MainWindowTitle } | Select-Object MainWindowHandle, MainWindowTitle
-
-if ($ventanas.Count -eq 0)
-{
-    Write-Host "`n[Vacio]" -ForegroundColor Red
+$windows = Get-Process | Where-Object { $_.MainWindowTitle } | Select-Object MainWindowHandle, MainWindowTitle
+if ($windows.Count -ne 0) {
+  $windows | Format-Table -AutoSize
 }
-else
-{
-    $ventanas | Format-Table -AutoSize
-}
-
 do {
-    $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+  $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 } while ($key.ControlKeyState -ne 0 -or $key.Character -eq 'v' -or $key.Character -eq 'V')
