f16528964896703861b034097725fd3c7fea7180
high on tantor <josephvenable@verizon.net>
2025-11-15 00:35:49 -0500

    Delete datafiles/SeeWindowIDs.ps1

--- datafiles/SeeWindowIDs.ps1
+++
@@ -1,7 +0,0 @@
-$windows = Get-Process | Where-Object { $_.MainWindowTitle } | Select-Object MainWindowHandle, MainWindowTitle
-if ($windows.Count -ne 0) {
-  $windows | Format-Table -AutoSize
-}
-do {
-  $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-} while ($key.ControlKeyState -ne 0 -or $key.Character -eq 'v' -or $key.Character -eq 'V')
